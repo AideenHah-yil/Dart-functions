629200656c38884b2aad621cafc75e3bfc7a3ca8
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-09 01:29:23 -0800

    adding task

--- function.dart
+++ function.dart
@@ -9,3 +9,18 @@ void main() {
     int sum = addTwo(a, b);
    print(sum); 
 }
+
+// Function to subtract two numbers
+double subtractTwo(double a, double b) {
+    double diff = a - b;
+    return diff;
+}
+
+void main() {
+    double a = 33.3;
+    double b = 22.2;
+    double diff = subtractTwo(a, b);
+
+    print(diff);
+}
+
