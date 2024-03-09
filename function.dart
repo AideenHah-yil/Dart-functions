8bb99e2604e4f56afb374b899ab922aa577eaf89
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-09 02:02:17 -0800

    fixing code

--- function.dart
+++ function.dart
@@ -3,13 +3,7 @@ int addTwo(int a, int b) {
     int sum = a + b;
     return sum;
 }
-// void main() {
-//     int a = 10;
-//     int b = 20;
 
-//     int sum = addTwo(a, b);
-//    print(sum); 
-// }
 
 // Function to subtract two numbers
 double subtractTwo(double num1, double num2) {
@@ -17,40 +11,24 @@ double subtractTwo(double num1, double num2) {
     return diff;
 }
 
-// void main() {
-//     double a = 33.3;
-//     double b = 22.2;
-//     double diff = subtractTwo(a, b);
-
-//     print(diff);
-// }
 
 //function to multiply two numbers
-int multiplyTwo(int a, int b) {
-    int mul = a * b;
+int multiplyTwo(int e, int f) {
+    int mul = e * f;
     return mul;
-}
- void main() {
-    int a = 9
-    int b = 3
-    int mul = multiplyTwo(a, b);
-
-    print(mul);
 }
 
 // function to divide two numbers
-int divideTwo(int a, int b) {
-    int div = a / b;
-    return div;
+double divideTwo(double g, double h) {
+    if (b != 0) {
+        double div = g / h;
+        return div;
+    } else {
+        print("Error: Cannot divide by zero");
+        return null;//to indicate error
+    }
 }
 
-void main() {
-    int a = 81;
-    int b = 3;
-    int div = divideTwo(a, b);
-
-    print(div);
-}
 
 // function stringlength 
 int stringlength(String text) {
@@ -71,8 +49,8 @@ String getFirstElement(String text) {
     return first;
 }
 
-void main() {
-    String text = "Hello";
-    String first = getFirstElement(text);
-    print(first);
-}
+// void main() {
+//     String text = "Hello";
+//     String first = getFirstElement(text);
+//     print(first);
+// }
